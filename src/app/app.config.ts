a5f5790c8636ad1d52fbedc2e77650f1d928add2
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-03-11 11:42:07 +0000

    complete job offers page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient} from "@angular/common/http";
 import {AuthInterceptor} from "./interceptors/auth/auth.interceptor";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, provideAnimationsAsync()
   ]
 };
--- src/app/job-offers/job-offers.component.ts
+++ src/app/job-offers/job-offers.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatTree, MatTreeNode} from "@angular/material/tree";
 
 @Component({
   selector: 'app-job-offers',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTree,
+    MatTreeNode
+  ],
   templateUrl: './job-offers.component.html',
   styleUrl: './job-offers.component.css'
 })
